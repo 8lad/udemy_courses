aa61c37753df59fa5996ade1ef18a118b8403e37
Vlad <vladisla1988@gmail.com>
2020-04-16 22:33:35 +0300

    start project

---
+++ main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// let test = 2;
+// let persone = {
+//     name: 'Josh',
+//     secondName: 'Lili',
+//     age: 34,
+//     isMaried: false
+// };
+// let arr = ['plum.png', 'apple.bmp', 'local.jpg'];
+// console.log('put this ' + arr[0], arr[2] + ' on your phone');
+// alert('hi kitty');
+// confirm(test + ' are you have this numbers?');
+// console.log(confirm(test + ' are you have this numbers?'));
+// let yo = prompt('do you have 17?', '');
+// console.log(yo);
+// console.log(typeof(null));
+// console.log('arr' + '-object');
+// console.log(3);
+// let incr = 10,
+//     decr = 10;
+// console.log(incr++);
+// console.log(decr-- % 7);
+// console.log(2 === '2');
+let isChecked = true,
+    isClose = false;
+console.log(isChecked || isClose);
